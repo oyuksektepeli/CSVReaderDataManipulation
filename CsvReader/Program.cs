f406acfa1ec3073c6c39a847b667f5bb23fd897f
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-11-06 19:28:57 +0300

    Changed foreach to for loop

--- CsvReader/Program.cs
+++ CsvReader/Program.cs
@@ -7,15 +7,17 @@ namespace CSVReaderDataManipulation
     {
         static void Main(string[] args)
         {
-            string filepath = @"C:\Repos\CSVReaderDataManipulation\CountryPopulations.csv";
+            string filepath = @"D:\Repos\CSVReaderDataManipulation\CountryPopulations.csv";
 
             Reader csvreader = new Reader(filepath);
 
             List<Country> countries = csvreader.ReadAllCountries();
             
 
-            foreach(Country country in countries)
+            //foreach(Country country in countries)
+            for (int i =0; i<countries.Count; i++)
             {
+                Country country = countries[i];
                 Console.WriteLine($"{Formatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
             }
             Console.WriteLine($"{countries.Count} countries");
