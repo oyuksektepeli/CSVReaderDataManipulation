ba7c3d15216d608db1a6995d27b492be6c2eb1cb
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-11-06 19:11:29 +0300

    Add project files.

---
+++ CsvReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSVReaderDataManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Repos\CsvReaderList\CountryPopulations.csv";
+
+            Reader csvreader = new Reader(filepath);
+
+            List<Country> countries = csvreader.ReadAllCountries();
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputindex = countries.FindIndex(x=>x.Population < 2_000_000);
+            countries.Insert(lilliputindex,lilliput);
+            countries.RemoveAt(lilliputindex);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{Formatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            Console.WriteLine($"{countries.Count} countries");
+        }
+    }
+}
