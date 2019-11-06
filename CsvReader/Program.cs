e2024e3ace3e09fbb166aec04c3f47ddf3fc5311
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-11-06 19:41:51 +0300

    List spesific number of Countries. Get User input.

--- CsvReader/Program.cs
+++ CsvReader/Program.cs
@@ -12,10 +12,19 @@ namespace CSVReaderDataManipulation
             Reader csvreader = new Reader(filepath);
 
             List<Country> countries = csvreader.ReadAllCountries();
-            
+
+            Console.WriteLine("Enter no. of countries to display> ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
+            {
+                Console.WriteLine("You must type in a +ve integer. Exiting");
+                return;
+            }
+
+            int maxToDisplay = Math.Min(userInput, countries.Count);
 
             //foreach(Country country in countries)
-            for (int i =0; i<countries.Count; i++)
+            for (int i =0; i<maxToDisplay; i++)
             {
                 Country country = countries[i];
                 Console.WriteLine($"{Formatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
